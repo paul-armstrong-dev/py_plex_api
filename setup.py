8b2b54c74f5235914b00a1f383afe1aeb1d452a0
Paul Armstrong <parmstrong@gitlab.com>
2021-07-16 15:44:36 +0200

    Bump version: 0.1.2 → 0.2.0

--- setup.py
+++ setup.py
@@ -56,6 +56,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/paul-armstrong-dev/py_plex_api',
-    version='0.1.2',
+    version='0.2.0',
     zip_safe=False,
 )
